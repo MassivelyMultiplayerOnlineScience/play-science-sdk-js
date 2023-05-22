6d1a74ba96655553cd9bb879e3f72da3c0ab81e6
Attila Szantner <attila.szantner@gmail.com>
2023-05-22 22:59:38 +0200

    Build

--- dist/v1/API.js
+++ dist/v1/API.js
@@ -45,7 +45,7 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
-import { interpolateTemplate } from 'src/lib/tools';
+import { interpolateTemplate } from '../lib/tools';
 import APIGames from './APIGames';
 import APIPlayers from './APIPlayers';
 import APIProjects from './APIProjects';
